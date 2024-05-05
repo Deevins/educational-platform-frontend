e81031186306d710dbf07e3e320caccf5af6808c
Viktor Samsonov <dezerqq26@gmail.com>
2024-05-05 04:28:38 +0300

    move sidebar to instructor layout

--- src/pages/instructor/InstructorCoursesPage.tsx
+++ src/pages/instructor/InstructorCoursesPage.tsx
@@ -1,134 +1,19 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { FiBook, FiCalendar, FiHome, FiSettings, FiUsers } from 'react-icons/fi'
+import React from 'react'
 import { NavLink } from 'react-router-dom'
 
+// TODO: after button clicked, redirect to /course/create and
+//  after 4 steps create course draft on backend and fetch after button 'Create course'
+//  clicked redirect to new course create page and info adding(instructor/course/5957360/manage/goals/) 5957360 - course id. this course is set as draft in status
 const InstructorCoursesPage: React.FC = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false)
-  const [activeItem, setActiveItem] = useState<number>(1)
-  const menuRef = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    const handleOutsideClick = (event: MouseEvent) => {
-      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
-        setIsMenuOpen(false)
-      }
-    }
-
-    document.addEventListener('mousedown', handleOutsideClick)
-
-    return () => {
-      document.removeEventListener('mousedown', handleOutsideClick)
-    }
-  }, [])
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen)
-  }
-
-  const handleItemClick = (itemId: number) => {
-    setActiveItem(itemId)
-  }
-
-  const handleMouseEnter = () => {
-    setIsMenuOpen(true)
-  }
-
-  const handleMouseLeave = () => {
-    setIsMenuOpen(false)
-  }
-
   return (
-    <div className='relative flex h-screen'>
-      {/* Sidebar */}
-      <div
-        ref={menuRef}
-        className={`fixed top-0 left-0 h-full w-20 md:w-30 bg-gray-800 text-white z-50 ${isMenuOpen ? 'md:w-96' : 'md:w-30'} transition-all duration-700`}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-      >
-        {/* Menu Button */}
-        <div className='md:hidden p-4'>
-          <button onClick={toggleMenu} className='flex items-center'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-6 w-6'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
-            >
-              {isMenuOpen ? (
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth={2}
-                  d='M6 18L18 6M6 6l12 12'
-                />
-              ) : (
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth={2}
-                  d='M4 6h16M4 12h16m-7 6h7'
-                />
-              )}
-            </svg>
-            <span className={`ml-2 ${isMenuOpen ? 'block' : 'hidden'}`}>
-              <FiHome />
-            </span>
-          </button>
-        </div>
-
-        {/* Sidebar Content */}
-        <div
-          className={`flex flex-col justify-center ${isMenuOpen ? 'block' : 'hidden'}`}
-        >
-          {/* Menu items */}
-          <div className='py-4'>
-            <MenuNavItem
-              title='Home'
-              icon={<FiHome size={20} />}
-              isActive={activeItem === 1}
-              onItemClick={() => handleItemClick(1)}
-              isVisible={isMenuOpen}
-            />
-            <MenuNavItem
-              title='Settings'
-              icon={<FiSettings size={20} />}
-              isActive={activeItem === 2}
-              onItemClick={() => handleItemClick(2)}
-              isVisible={isMenuOpen}
-            />
-            <MenuNavItem
-              title='Courses'
-              icon={<FiBook size={20} />}
-              isActive={activeItem === 3}
-              onItemClick={() => handleItemClick(3)}
-              isVisible={isMenuOpen}
-            />
-            <MenuNavItem
-              title='Users'
-              icon={<FiUsers size={20} />}
-              isActive={activeItem === 4}
-              onItemClick={() => handleItemClick(4)}
-              isVisible={isMenuOpen}
-            />
-            <MenuNavItem
-              title='Calendar'
-              icon={<FiCalendar size={20} />}
-              isActive={activeItem === 5}
-              onItemClick={() => handleItemClick(5)}
-              isVisible={isMenuOpen}
-            />
-          </div>
-        </div>
-      </div>
-
+    <div className='relative flex'>
       {/* Main Content */}
       <div className='flex-1 flex flex-col items-center pl-20 md:pl-30'>
         <div className='shadow-xl border-2 border-gray-100 lg:w-8/12 h-32 flex justify-between items-center mt-20 px-10'>
           <h1 className='text-xl pr-16 lg:pr-32 py-32'>Перейти к созданию курса</h1>
           <button className='hover:bg-blue-600 text-white font-bold lg:px-20 py-4 px-10 bg-black'>
             <NavLink to='/create-course'>Создать курс</NavLink>
+            {/* // TODO: check comment on top of page */}
           </button>
         </div>
 
@@ -139,7 +24,38 @@ const InstructorCoursesPage: React.FC = () => {
         </div>
 
         <div className='flex flex-col items-center'>
-          <div className={'flex justify-center'}>
+          <div className={'flex justify-center w-[92%] ml-28 lg:ml-0'}>
+            <div className='shadow-xl border-2 border-gray-100 pt-4 pl-4 pb-4 pr-2 my-4 mr-32 relative flex flex-col  w-[80%]'>
+              <div className='flex flex-col lg:flex-row justify-center'>
+                <div className='flex-shrink-0 lg:order-1 pr-8'>
+                  <img
+                    src='https://s.udemycdn.com/instructor/dashboard/build-audience.jpg'
+                    alt='asd'
+                    className='hidden lg:block h-[80%] float-right'
+                  />
+                </div>
+                <div className='ml-4 w-[50%]'>
+                  <h1 className='text-center mb-4 font-medium'>
+                    Создайте привлекательный курс
+                  </h1>
+                  <p>
+                    Вне зависимости от вашего опыта в преподавании вы можете сделать свой
+                    курс увлекательным. Мы подготовили подборку ресурсов и передовых
+                    методик, которые помогут вам достичь новых высот и выйти на новый
+                    уровень.
+                  </p>
+                </div>
+              </div>
+              <div className='flex justify-center mt-auto'>
+                <NavLink to={'/instructor/resources/teaching'}>
+                  <button className='font-bold py-2 px-4 text-black hover:underline'>
+                    Приступите к работе
+                  </button>
+                </NavLink>
+              </div>
+            </div>
+          </div>
+          <div className={'flex justify-center ml-32 lg:ml-0'}>
             <div className='shadow-xl border-2 border-gray-100 pt-4 pl-4 pb-4 pr-2 my-4 mr-32 w-1/3 relative'>
               <div className='flex flex-col lg:flex-row'>
                 <div className='flex-shrink-0 lg:order-1 pr-8'>
@@ -162,39 +78,34 @@ const InstructorCoursesPage: React.FC = () => {
                 </div>
               </div>
               <div className='flex justify-center'>
-                <NavLink to={''}>
+                <NavLink to={'/instructor/resources/teaching'}>
                   <button className=' font-bold py-2 px-4 text-black hover:underline'>
-                    Кнопка 1
+                    Приступите к работе
                   </button>
                 </NavLink>
               </div>
             </div>
 
-            <div className='shadow-xl border-2 border-gray-100 pt-4 pl-4 pb-4 pr-2 my-4 mr-32 w-1/3 relative'>
+            <div className='shadow-xl border-2 border-gray-100 pt-4 pl-4 pb-4 pr-2 my-4 mr-32 w-1/3 relative flex flex-col'>
               <div className='flex flex-col lg:flex-row'>
                 <div className='flex-shrink-0 lg:order-1 pr-8'>
                   <img
-                    src='https://s.udemycdn.com/instructor/dashboard/engaging-course.jpg'
+                    src='https://s.udemycdn.com/instructor/dashboard/build-audience.jpg'
                     alt='asd'
                     className='hidden lg:block h-[80%] float-right'
                   />
                 </div>
                 <div className='ml-4 flex-grow'>
                   <h1 className='text-center mb-4 font-medium'>
-                    Создайте привлекательный курс
+                    Собирайте собственную аудиторию
                   </h1>
-                  <p>
-                    Вне зависимости от вашего опыта в преподавании вы можете сделать свой
-                    курс увлекательным. Мы подготовили подборку ресурсов и передовых
-                    методик, которые помогут вам достичь новых высот и выйти на новый
-                    уровень.
-                  </p>
+                  <p>Сделайте свой курс успешным, формируя собственную аудиторию.</p>
                 </div>
               </div>
-              <div className='flex justify-center'>
+              <div className='flex justify-center mt-auto'>
                 <NavLink to={''}>
-                  <button className=' font-bold py-2 px-4 text-black hover:underline'>
-                    Кнопка 1
+                  <button className='font-bold py-2 px-4 text-black hover:underline'>
+                    Приступите к работе
                   </button>
                 </NavLink>
               </div>
@@ -220,41 +131,4 @@ const InstructorCoursesPage: React.FC = () => {
   )
 }
 
-interface MenuNavItemProps {
-  title: string
-  icon: React.ReactNode
-  isActive: boolean
-  onItemClick: () => void
-  isVisible: boolean
-}
-
-const MenuNavItem: React.FC<MenuNavItemProps> = ({
-  title,
-  icon,
-  isActive,
-  onItemClick,
-  isVisible,
-}) => {
-  return (
-    <div
-      className={`flex items-center px-4 py-2 cursor-pointer relative ${isActive ? 'bg-gray-700' : ''} ${
-        isVisible ? 'transition-opacity duration-1000 opacity-100' : ''
-      } hover:bg-gray-700 `}
-      onClick={onItemClick}
-    >
-      <div
-        className={`absolute left-0 w-2 h-full bg-blue-500 ${isActive ? '' : 'hidden'}`}
-      ></div>
-      <p
-        className={`flex items-center px-4 py-2 cursor-pointer relative ${isActive ? 'bg-gray-700' : ''} ${
-          isVisible ? 'transition-opacity duration-1000 opacity-100' : ''
-        } hover:bg-gray-700 `}
-      >
-        {icon}
-      </p>
-
-      <span className='ml-2 text-lg block'>{title}</span>
-    </div>
-  )
-}
 export default InstructorCoursesPage
